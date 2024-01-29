831acc9be3c69291e99e6f73462a57a9ff98d729
abdealijaroli <abdealiburu03@gmail.com>
2024-01-29 18:05:03 +0530

    init

---
+++ contains_duplicate/main.go
@@ -0,0 +1,14 @@
+package contains_duplicate
+
+func containsDuplicate(nums []int) bool {
+    set := map[int]struct{}{}
+
+	for _, n := range nums {
+		if _, ok := set[n]; ok {
+			return true
+		}
+		set[n] = struct{}{}
+	}
+    
+	return false  
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module leetgo/contains_duplicate
+
+go 1.21.5
